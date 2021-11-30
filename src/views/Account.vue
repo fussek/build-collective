8ba4f7d9eed1e21dce24be5f3fb4ff276ecb4975
patryk.fussek.1996 <patryk.fussek@campus.tu-berlin.de>
2021-11-30 13:36:09 +0100

    Run npm lint

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -21,84 +21,116 @@
         :subtitle="`${balance} ETH\t\t \nYour balance: ${account.balance} Tokens\t\t \nYour address: ${address}`"
         :gradient="true"
       >
-
         <div class="explanations">
-          <button class="simple-button" @click="addTokens">ADD 200 TOKENS</button>
+          <button class="simple-button" @click="addTokens">
+            ADD 200 TOKENS
+          </button>
         </div>
-        <div class="explanations" @click="toggleShowProjects()" style="cursor: pointer; font-size: 20px; margin: 10px">
+        <div
+          class="explanations"
+          @click="toggleShowProjects()"
+          style="cursor: pointer; font-size: 20px; margin: 10px"
+        >
           Click here to see the list of projects
-          <div style="font-size: 15px; margin: 5px">TOTAL N° PROJECTS: {{ projects.length }} projects.</div>
-           </div>
-        <div class="explanations" @click="toggleShowEnterprises()" style="cursor: pointer; font-size: 20px; margin: 10px">
-          Click here to see the list of enterprises.
-          <div style="font-size: 15px; margin: 5px">TOTAL N° ENTERPRISES: {{ enterprises.length }} enterprises. </div>
+          <div style="font-size: 15px; margin: 5px">
+            TOTAL N° PROJECTS: {{ projects.length }} projects.
           </div>
+        </div>
+        <div
+          class="explanations"
+          @click="toggleShowEnterprises()"
+          style="cursor: pointer; font-size: 20px; margin: 10px"
+        >
+          Click here to see the list of enterprises.
+          <div style="font-size: 15px; margin: 5px">
+            TOTAL N° ENTERPRISES: {{ enterprises.length }} enterprises.
+          </div>
+        </div>
       </card>
       <card
-          class="create-card"
-          title="Create a project"
-          subtitle="Create a new project with tokens balance and contributors"
-          :gradient="true"
+        class="create-card"
+        title="Create a project"
+        subtitle="Create a new project with tokens balance and contributors"
+        :gradient="true"
       >
-        <div v-if="!projectCreationTrigger" @click="toggleProjectCreation" style="cursor: pointer; margin: 25px 5px 20px 20px; font-size: 20px" >Click to add a new project to your account</div>
+        <div
+          v-if="!projectCreationTrigger"
+          @click="toggleProjectCreation"
+          style="cursor: pointer; margin: 25px 5px 20px 20px; font-size: 20px"
+        >
+          Click to add a new project to your account
+        </div>
         <div v-if="projectCreationTrigger">
           <input
-              type="text"
-              v-model="projectName"
-              class="input-username"
-              placeholder="Name of the project"
+            type="text"
+            v-model="projectName"
+            class="input-username"
+            placeholder="Name of the project"
           />
           <input
-              type="number"
-              v-model="projectTokensBalance"
-              class="input-username"
-              placeholder="Tokens balance"
+            type="number"
+            v-model="projectTokensBalance"
+            class="input-username"
+            placeholder="Tokens balance"
           />
           <input
-              type="text"
-              v-model="projectContributorsList"
-              class="input-username"
-              placeholder="Contributors separated by comma"
+            type="text"
+            v-model="projectContributorsList"
+            class="input-username"
+            placeholder="Contributors separated by comma"
           />
-          <button class="simple-button" @click="createProject()"> SUBMIT !</button>
-          <button class="simple-button" @click="toggleProjectCreation"> CANCEL </button>
+          <button class="simple-button" @click="createProject()">
+            SUBMIT !
+          </button>
+          <button class="simple-button" @click="toggleProjectCreation">
+            CANCEL
+          </button>
         </div>
-
       </card>
       <card
-          class="create-card"
-          title="Create an enterprise"
-          subtitle="Create a new enterprise with tokens balance and contributors"
-          :gradient="true"
+        class="create-card"
+        title="Create an enterprise"
+        subtitle="Create a new enterprise with tokens balance and contributors"
+        :gradient="true"
       >
-        <div v-if="!enterpriseCreationTrigger" @click="toggleEnterpriseCreation" style="cursor: pointer; margin: 25px 5px 20px 20px; font-size: 20px">Click to add a new enterprise</div>
+        <div
+          v-if="!enterpriseCreationTrigger"
+          @click="toggleEnterpriseCreation"
+          style="cursor: pointer; margin: 25px 5px 20px 20px; font-size: 20px"
+        >
+          Click to add a new enterprise
+        </div>
         <div v-if="enterpriseCreationTrigger">
           <input
-              type="text"
-              v-model="enterpriseName"
-              class="input-username"
-              placeholder="Name of the enterprise"
+            type="text"
+            v-model="enterpriseName"
+            class="input-username"
+            placeholder="Name of the enterprise"
           />
           <input
-              type="number"
-              v-model="enterpriseTokensBalance"
-              class="input-username"
-              placeholder="Tokens balance"
+            type="number"
+            v-model="enterpriseTokensBalance"
+            class="input-username"
+            placeholder="Tokens balance"
           />
           <input
-              type="text"
-              v-model="enterpriseContributorsList"
-              class="input-username"
-              placeholder="Contributors separated by comma"
+            type="text"
+            v-model="enterpriseContributorsList"
+            class="input-username"
+            placeholder="Contributors separated by comma"
           />
 
-          <button class="simple-button" @click="createEnterpriseAccount()"> SUBMIT !</button>
-          <button class="simple-button" @click="toggleEnterpriseCreation"> CANCEL </button>
+          <button class="simple-button" @click="createEnterpriseAccount()">
+            SUBMIT !
+          </button>
+          <button class="simple-button" @click="toggleEnterpriseCreation">
+            CANCEL
+          </button>
         </div>
       </card>
     </div>
   </div>
-  <div class="home" v-if="account && showProjects ">
+  <div class="home" v-if="account && showProjects">
     <div class="card-home-wrapper">
       <card
         :title="`Your username: ${account.username}`"
@@ -114,7 +146,8 @@
 
         <div v-if="projectsViewSwitch">
           <div v-for="project in this.projects" v-bind:key="project.name">
-            <card style="margin: 10px"
+            <card
+              style="margin: 10px"
               :title="`Project name: ${project.name}`"
               :subtitle="`
                   Owner of the project: ${project.owner.username}
@@ -123,17 +156,18 @@
             <div style="margin: 10px">
               Enter number of tokens you wish to donate:
               <input
-                  type="number"
-                  v-model="projectDonation"
-                  placeholder="Amount"
+                type="number"
+                v-model="projectDonation"
+                placeholder="Amount"
               />
-              <button @click="donateToProject(project)"> DONATE </button>
+              <button @click="donateToProject(project)">DONATE</button>
             </div>
           </div>
         </div>
         <div v-if="!projectsViewSwitch">
           <div v-for="project in this.projects" v-bind:key="project.name">
-            <card style="margin: 10px"
+            <card
+              style="margin: 10px"
               v-if="project.owner.username == account.username"
               :title="`Project name: ${project.name}`"
               :subtitle="`
@@ -145,25 +179,29 @@
       </card>
     </div>
   </div>
-  <div class="home" v-if="account && showEnterprises ">
+  <div class="home" v-if="account && showEnterprises">
     <div class="card-home-wrapper">
       <card
-          :title="`Your username: ${account.username}`"
-          subtitle="Enterprises list:"
-          :gradient="true"
+        :title="`Your username: ${account.username}`"
+        subtitle="Enterprises list:"
+        :gradient="true"
       >
-        <button class="simple-button" @click=toggleShowEnterprises()> BACK </button>
+        <button class="simple-button" @click="toggleShowEnterprises()">
+          BACK
+        </button>
 
-        <div v-for="enterprise in this.enterprises" v-bind:key="enterprise.name">
+        <div
+          v-for="enterprise in this.enterprises"
+          v-bind:key="enterprise.name"
+        >
           <card
-              :title="`Enterprise name: ${enterprise.name}`"
-              :subtitle="`Balance: ${enterprise.balance}`"
+            :title="`Enterprise name: ${enterprise.name}`"
+            :subtitle="`Balance: ${enterprise.balance}`"
           />
         </div>
       </card>
     </div>
   </div>
-
 </template>
 
 <script lang="ts">
@@ -181,17 +219,33 @@ export default defineComponent({
     return { address, contract, balance }
   },
   data() {
-    const account = null;
-    const username = '';
-    const project = null;
-    let projectName = null;
-    let projectTokensBalance = 0;
-    let projectDonation = 0;
-    let enterpriseName = null;
-    let enterpriseTokensBalance = 0;
-    const projects: any[] = [];
-    const enterprises: any[] = [];
-    return { account, username, project, projectName, projectTokensBalance, projectDonation, enterpriseName, enterpriseTokensBalance, projects, enterprises, projectCreationTrigger:false, enterpriseCreationTrigger: false, showProjects: false, projectsViewSwitch: false, showEnterprises: false }
+    const account = null
+    const username = ''
+    const project = null
+    let projectName = null
+    let projectTokensBalance = 0
+    let projectDonation = 0
+    let enterpriseName = null
+    let enterpriseTokensBalance = 0
+    const projects: any[] = []
+    const enterprises: any[] = []
+    return {
+      account,
+      username,
+      project,
+      projectName,
+      projectTokensBalance,
+      projectDonation,
+      enterpriseName,
+      enterpriseTokensBalance,
+      projects,
+      enterprises,
+      projectCreationTrigger: false,
+      enterpriseCreationTrigger: false,
+      showProjects: false,
+      projectsViewSwitch: false,
+      showEnterprises: false,
+    }
   },
   methods: {
     async updateAccount() {
@@ -211,23 +265,23 @@ export default defineComponent({
       await this.updateAccount()
     },
     toggleProjectCreation() {
-      this.projectCreationTrigger = !this.projectCreationTrigger;
+      this.projectCreationTrigger = !this.projectCreationTrigger
     },
     toggleEnterpriseCreation() {
-      this.enterpriseCreationTrigger = !this.enterpriseCreationTrigger;
+      this.enterpriseCreationTrigger = !this.enterpriseCreationTrigger
     },
     changeProjectsView() {
       this.projectsViewSwitch = !this.projectsViewSwitch
     },
     async toggleShowProjects() {
-      await this.getProjects();
-      this.showProjects = !this.showProjects;
+      await this.getProjects()
+      this.showProjects = !this.showProjects
     },
     async getProjects() {
       this.projects = []
       const { contract } = this
       const projects = await contract.methods.getProjects().call()
-      if (projects.length > 0){
+      if (projects.length > 0) {
         for (let n = 0; n < projects.length; n++) {
           let name = projects[n].name
           let balance = projects[n].balance
@@ -236,54 +290,67 @@ export default defineComponent({
             id: n,
             name: name,
             balance: balance,
-            owner: owner
+            owner: owner,
           })
         }
       }
     },
     async toggleShowEnterprises() {
-      await this.getUserEnterpriseIds();
-      this.showEnterprises = !this.showEnterprises;
+      await this.getUserEnterpriseIds()
+      this.showEnterprises = !this.showEnterprises
     },
     async getUserEnterpriseIds() {
-      this.enterprises = [];
-      const { contract } = this;
-      const enterprisesIds = await contract.methods.getUserEnterpriseIds().call();
-      if (enterprisesIds.length > 0){
+      this.enterprises = []
+      const { contract } = this
+      const enterprisesIds = await contract.methods
+        .getUserEnterpriseIds()
+        .call()
+      if (enterprisesIds.length > 0) {
         for (const enterprisesId of enterprisesIds) {
-          const enterprise = await contract.methods.getEnterprise(enterprisesId).call();
-          let name = enterprise.name;
-          let balance = enterprise.balance;
-          let owner = enterprise.owner;
+          const enterprise = await contract.methods
+            .getEnterprise(enterprisesId)
+            .call()
+          let name = enterprise.name
+          let balance = enterprise.balance
+          let owner = enterprise.owner
           this.enterprises.push({
             id: enterprise.id,
             name: name,
             balance: balance,
-            owner: owner
+            owner: owner,
           })
         }
       }
     },
     async createProject() {
       const { contract, project } = this
-      await contract.methods.createProject(this.projectName, 0, this.projectTokensBalance).send()
+      await contract.methods
+        .createProject(this.projectName, 0, this.projectTokensBalance)
+        .send()
       await this.updateAccount()
-      this.projectCreationTrigger = false;
+      this.projectCreationTrigger = false
       await this.getProjects()
     },
     async createEnterpriseAccount() {
       const { contract } = this
-      await contract.methods.createEnterpriseAccount(this.enterpriseName, this.enterpriseTokensBalance).send()
+      await contract.methods
+        .createEnterpriseAccount(
+          this.enterpriseName,
+          this.enterpriseTokensBalance
+        )
+        .send()
       await this.updateAccount()
-      this.enterpriseCreationTrigger = false;
+      this.enterpriseCreationTrigger = false
       await this.getUserEnterpriseIds()
     },
-    async donateToProject(project : any) {
+    async donateToProject(project: any) {
       const { contract } = this
-      await contract.methods.supportProject(project.id, this.projectDonation).send()
+      await contract.methods
+        .supportProject(project.id, this.projectDonation)
+        .send()
       await this.updateAccount()
-      this.projectCreationTrigger = false;
-      this.projectDonation = 0;
+      this.projectCreationTrigger = false
+      this.projectDonation = 0
       await this.getProjects()
     },
   },
@@ -331,7 +398,7 @@ export default defineComponent({
   cursor: pointer;
 }
 
-.simple-button{
+.simple-button {
   margin: 25px 5px 20px 20px;
   display: inline-block;
   border: none;
@@ -341,7 +408,6 @@ export default defineComponent({
   text-decoration: none;
   font-size: 15px;
   font-family: inherit;
-
 }
 .input-username {
   background: transparent;
